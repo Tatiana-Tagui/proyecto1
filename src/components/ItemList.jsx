ac347b476e9d1ebc22f9cc135d7e8c6b9dbca308
proyecto1 <tati1bustos1@gmail.com>
2023-02-21 16:08:12 -0300

    se agrega el footer en index.html y sus estilos

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -20,11 +20,181 @@ const ItemList = () => {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A velit, asperiores dolor voluptatibus facere veniam recusandae fugit praesentium quis et",
     },
+    {
+      id: 2,
+      nombre: "Car 2",
+      price: 1200200,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A velit, asperiores dolor voluptatibus facere veniam recusandae fugit praesentium quis et",
+    },
+    {
+      id: 3,
+      nombre: "Car 3",
+      price: 1200200,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A velit, asperiores dolor voluptatibus facere veniam recusandae fugit praesentium quis et",
+    },
+    {
+      id: 4,
+      nombre: "Car 4",
+      price: 1200200,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A velit, asperiores dolor voluptatibus facere veniam recusandae fugit praesentium quis et",
+    },
+    {
+      id: 5,
+      nombre: "Car 5",
+      price: 1200200,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A velit, asperiores dolor voluptatibus facere veniam recusandae fugit praesentium quis et",
+    },
+    {
+      id: 6,
+      nombre: "Car 6",
+      price: 1200200,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A velit, asperiores dolor voluptatibus facere veniam recusandae fugit praesentium quis et",
+    },
   ];
 
   return (
     <>
-      <Card maxW="sm">
+      <Card id="car1" maxW="sm">
+        <CardBody>
+          <Image
+            src={imgUrl}
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Living room Sofa</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+            <Text color="blue.600" fontSize="2xl">
+              $450
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid">Buy now</Button>
+            <Button variant="ghost">Add to cart</Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+      <Card id="car2" maxW="sm">
+        <CardBody>
+          <Image
+            src={imgUrl}
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Living room Sofa</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+            <Text color="blue.600" fontSize="2xl">
+              $450
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid">Buy now</Button>
+            <Button variant="ghost">Add to cart</Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+      <Card id="car3" maxW="sm">
+        <CardBody>
+          <Image
+            src={imgUrl}
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Living room Sofa</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+            <Text color="blue.600" fontSize="2xl">
+              $450
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid">Buy now</Button>
+            <Button variant="ghost">Add to cart</Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+      <Card id="car4" maxW="sm">
+        <CardBody>
+          <Image
+            src={imgUrl}
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Living room Sofa</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+            <Text color="blue.600" fontSize="2xl">
+              $450
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid">Buy now</Button>
+            <Button variant="ghost">Add to cart</Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+      <Card id="car5" maxW="sm">
+        <CardBody>
+          <Image
+            src={imgUrl}
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Living room Sofa</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+            <Text color="blue.600" fontSize="2xl">
+              $450
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid">Buy now</Button>
+            <Button variant="ghost">Add to cart</Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+      <Card id="car6" maxW="sm">
         <CardBody>
           <Image
             src={imgUrl}
